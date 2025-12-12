948274f69a21fd308ab9a2c64c92a1badc548098
Ramiro Graca <valentinramirograca123@gmail.com>
2025-12-12 19:15:29 -0300

    Estructura HTML y diseño CSS terminado. Validación JS terminada.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,84 @@
-import { createElement } from "./modules/dom.js";
-import { log } from "./modules/utils.js";
+import { nameValidation } from "./modules/utils.js";
+import { emailValidation } from "./modules/utils.js";
+import { phoneValidation } from "./modules/utils.js";
+import { subjectValidation } from "./modules/utils.js";
+import { messageValidation } from "./modules/utils.js";
 
-document.getElementById("btn-test").addEventListener("click", () => {
-    const el = createElement("p", "Este párrafo fue creado dinámicamente.");
-    document.getElementById("app").appendChild(el);
-    log("Evento ejecutado.");
-});
+let states = {name: false, email: false, phone:false, subject:false, message:false, terms:false};
+
+const inputs = document.querySelectorAll('input');
+
+
+
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const inputPhone = document.getElementById("phone");
+
+const inputSubject = document.getElementById("subject");
+const inputMessage = document.getElementById("message");
+
+
+const inputTerms = document.getElementById("terms");
+
+const contactForm = document.getElementById("contactForm");
+const submitButton = document.getElementById("submitBtn");
+
+
+contactForm.addEventListener("input", validateInput);
+
+function validateInput(e) {
+    const inputText = e.target.value;
+    
+    let idTag = e.target.id;
+
+    switch (idTag) {
+        case "name":
+            states.name = nameValidation(inputText);
+            break;
+        case "email":
+            states.email = emailValidation(inputText);
+            
+            break;
+        case "phone":
+            states.phone = phoneValidation(inputText);
+            break;
+        case "subject":
+            states.subject = subjectValidation(inputText);
+            break;
+        case "message":
+            states.message = messageValidation(inputText);
+            break;
+        case "terms":
+            states.terms = e.target.checked; 
+            break;
+        case "submitBtn":
+            break;
+    }
+
+    let element = e.target;
+    if (element.type != "checkbox") {
+        if (element.value != "") {
+            if (!states[element.id]) {
+                element.classList.remove("valid");
+                element.classList.add("invalid");
+            } else {
+                element.classList.remove("invalid");
+                element.classList.add("valid");    
+            }
+        } else {
+            element.classList.remove("invalid");
+            element.classList.remove("valid");
+        }
+    } else {
+        if (element.checked) {
+            element.classList.remove("invalid");
+            element.classList.add("valid");
+        } else {
+            element.classList.add("invalid");
+            element.classList.remove("valid");
+        }
+    }
+
+    let formState = Object.values(states).every(v => v === true);
+    submitButton.disabled = formState ? false : true;
+}
--- src/js/modules/utils.js
+++ src/js/modules/utils.js
@@ -1,3 +1,53 @@
-export function log(...args) {
-    console.log("[LOG]:", ...args);
+export function nameValidation(name) {
+    //length > 2
+    let length = name.length <= 2 ? false : true;
+    
+    //no spaces
+    let noSpace = !name.includes(" ");
+    
+    //not numnbers 
+    let noNumbers = !/[0-9]/.test(name);
+    
+    //not special char
+    let noSpecial = !/[!@#$%^&*()_\-+=\[\]{};:'",.<>\/?\\|`~]/.test(name);
+    
+    
+    let output = noSpecial && noNumbers && noSpace && length; 
+
+    return output;
+ }
+
+export function emailValidation(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+
+export function phoneValidation(phone) {
+  // allows +54 11 1234-5678, (011) 4567-8901, 1123456789, etc.
+  const regex = /^[+\d\s()-]{7,20}$/;
+
+  // Also we check for enough real numbers
+  const onlyNumbers = phone.replace(/\D/g, "");
+  const quantity = onlyNumbers.length;
+
+  return regex.test(phone) && quantity >= 7 && quantity <= 15;
+}
+
+
+
+export function subjectValidation (subject) {
+  //no empty strings
+  if (!subject || !subject.trim()) return false;
+
+  // Between 3 and 100 visible char
+  const limpio = subject.trim();
+  return limpio.length >= 3 && limpio.length <= 100;
+}
+
+
+export function messageValidation(message) {
+  if (!message || !message.trim()) return false;
+
+  const clean = message.trim();
+  return clean.length >= 10 && clean.length <= 1000;
 }
